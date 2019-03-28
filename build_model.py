da8c393ee7998911eddec5b5b44729614b574981
dksmagalhaes <dksmagalhaes@inf.ufpr.br>
2019-03-28 12:57:22 -0700

    Initial COmmit

---
+++ build_model.py
@@ -0,0 +1,10 @@
+import gensim
+import pandas as pd
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    link_u_imbd = 'datasets/imdb-unlabeled.txt'
+    link_train = 'datasets/train.txt'
+    link_test = 'datasets/test.txt'
